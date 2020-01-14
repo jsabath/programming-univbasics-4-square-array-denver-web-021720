310c290bf8d6aee0298cfb2539f6722ea45c955c
Justin Sabath <justin.sabath@aol.com>
2020-01-14 18:08:25 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(numbers)
   array = []
   count = 0
   while count < numbers.length do
-    array[count] = numbers[i] ** 2
+    array[count] = numbers[count] ** 2
     count += 1
   end
     array
